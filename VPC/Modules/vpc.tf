bb3d2956d9e4d925b47f607817bcd6afc2444aa0
Sneh Pratik <80665890+ItsSneh@users.noreply.github.com>
2025-09-16 23:34:32 +0530

    Create vpc.tf

---
+++ VPC/Modules/vpc.tf
@@ -0,0 +1,83 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "$(var.project_name)-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vps.main.id
+  tags = {
+    Name = "$(var.project_name)-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vps.main.id
+  cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availablity_zone = var.availabilty_zone
+  tags = {
+    Name = "$(var.project_name)-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vps.main.id
+  cidr_block = var.private_subnet_cidr
+  availablity_zone = var.availabilty_zone
+  tags = {
+    Name = "$(var.project_name)-private-subnet"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = vpc
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public.id
+  tags = {
+    Name = "$(var.project_name)-nat-gateway"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vps.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "$(var.project_name)-public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vps.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "$(var.project_name)-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "private_rta" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private_route.id
+}
